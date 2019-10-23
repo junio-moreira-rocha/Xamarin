654e64afac969d570d12709bd6230532a4d24f63
Junio Moreira da Rocha <junio-moreira@hotmail.com>
2019-10-22 21:29:10 -0300

    Commit inicial

---
+++ ConsultaClimaApp/ConsultaClimaApp/Pages/CidadeFavoritaPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using ConsultaClimaApp.Clients;
+using ConsultaClimaApp.Models;
+using ConsultaClimaApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConsultaClimaApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CidadeFavoritaPage : ContentPage
+    {
+        public CidadeFavoritaPage()
+        {
+            InitializeComponent();
+            CarregarInformacoes();
+            BindingContext = new CidadeFavoritaViewModel();
+
+        }
+
+        async void CarregarInformacoes()
+        {
+            var list = ((App)Application.Current).Conexao.Query<Favorito>("SELECT IdCidade, NomeCidade, Clima, Temperatura from Favoritos");
+            if (list.Count == 0)
+            {
+                await DisplayAlert("Olá!", "Você ainda não possui registros nos favoritos", "Ok");
+            }
+            else
+            {
+                ListView.ItemsSource = list;
+            }
+        }
+
+        async void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+            var favorito = e.SelectedItem as Favorito;
+            var clima = OpenWeatherMapClient.BuscarClima(favorito.IdCidade);
+
+            await Navigation.PushAsync(new DetalheCidadePage(clima));
+        }
+    }
+}
---
+++ ConsultaClimaApp/ConsultaClimaApp/Pages/DetalheCidadePage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ConsultaClimaApp.Models;
+using ConsultaClimaApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConsultaClimaApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetalheCidadePage : ContentPage
+    {
+        public DetalheCidadePage(Clima clima)
+        {
+            InitializeComponent();
+
+            BindingContext = new DetalheCidadeViewModel(clima);
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var idCidade = lblIdCidade.Text;
+            var nomeCidade = lblNomeCidade.Text;
+            var temperatura = lblTemperatura.Text;
+            var descricao = lblDescricaoTempo.Text;
+
+            var favorito = ((App)Application.Current).Conexao.Query<Clima>($"SELECT * from Favoritos WHERE IdCidade = {idCidade}");
+            if (favorito.Count == 0)
+            {
+                var query = $"INSERT INTO Favoritos (IdCidade, NomeCidade, Clima, Temperatura) VALUES ({idCidade}, '{nomeCidade}', '{descricao}', '{temperatura}')";
+                ((App)Application.Current).Conexao.Execute(query);
+                await Navigation.PushAsync(new CidadeFavoritaPage());
+            }
+            else
+            {
+                await App.Current.MainPage.DisplayAlert("Olá!", "Você já favoritou essa cidade.", "Ok");
+            }
+        }
+    }
+}
